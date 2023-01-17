8c2610e00882824bb7f53f7cce0766930d32f2a3
Bruno Silva Carvalho <47616928+brunocarvalhs@users.noreply.github.com>
2023-01-17 19:31:53 -0300

    Update version

--- buildSrc/src/main/kotlin/config/AndroidConfig.kt
+++ buildSrc/src/main/kotlin/config/AndroidConfig.kt
@@ -8,8 +8,8 @@ object AndroidConfig {
     const val TARGET_SDK_VERSION = 33
 
 
-    const val VERSION_CODE = 1
-    const val VERSION_NAME = "1.0"
+    const val VERSION_CODE = 2
+    const val VERSION_NAME = "2.0"
 
     const val APP_ID = "br.com.brunocarvalhs.paguei"
     const val TEST_INSTRUMENTATION_RUNNER = "androidx.test.runner.AndroidJUnitRunner"
