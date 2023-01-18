aa4057d51bcfe6ae968aad6db9555d313e76e317
Bruno Silva Carvalho <47616928+brunocarvalhs@users.noreply.github.com>
2023-01-17 21:26:08 -0300

    Feature/flow costs (#10)
    * Criação de fluxo de build
    
    * Update build.yml
    
    * Configuração do action build
    
    * Configuração do action build
    
    * Update build.yml
    
    * Mudança de tema do app
    
    * Mudança de tema do app e logo
    
    * Renomeando aplicativo
    
    * Mudança do layout do login
    
    * Mudança do layout do login
    
    * Renomeando aplicativo
    
    * Removendo .idea
    
    * Logout implementado
    
    * Refatorando código para melhor transição entre telas
    
    * Update version
    
    * Atualizando projeto
    
    * Atualizando projeto
    
    * Update version code
    
    * Removendo arquivo da pasta kotlin
    
    * Atualizando .idea

--- app/src/main/kotlin/br/com/brunocarvalhs/paguei/features/billet_registration/barcode_scanner/BilletRegistrationBarcodeScannerViewState.kt
+++
@@ -1,7 +0,0 @@
-package br.com.brunocarvalhs.paguei.features.billet_registration.barcode_scanner
-
-sealed class BilletRegistrationBarcodeScannerViewState {
-    object Loading : BilletRegistrationBarcodeScannerViewState()
-    data class Success(val code: String) : BilletRegistrationBarcodeScannerViewState()
-    data class Error(val message: String?) : BilletRegistrationBarcodeScannerViewState()
-}
--- app/src/main/kotlin/br/com/brunocarvalhs/paguei/features/homes/register/HomesRegisterViewState.kt
+++
@@ -1,7 +0,0 @@
-package br.com.brunocarvalhs.paguei.features.homes.register
-
-sealed class HomesRegisterViewState {
-    object Loading : HomesRegisterViewState()
-    object Success : HomesRegisterViewState()
-    data class Error(val error: String?) : HomesRegisterViewState()
-}
