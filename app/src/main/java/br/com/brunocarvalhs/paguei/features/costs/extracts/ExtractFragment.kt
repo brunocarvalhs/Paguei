c7a3ef0839e61339ee26d62527d704c0c09c9cde
Bruno Silva Carvalho <47616928+brunocarvalhs@users.noreply.github.com>
2023-01-17 21:50:06 -0300

    Listeners do Search (#12)
    * Criação de fluxo de build
    
    * Update build.yml
    
    * Configuração do action build
    
    * Configuração do action build
    
    * Update build.yml
    
    * Mudança de tema do app
    
    * Mudança de tema do app e logo
    
    * Renomeando aplicativo
    
    * Mudança do layout do login
    
    * Mudança do layout do login
    
    * Renomeando aplicativo
    
    * Removendo .idea
    
    * Logout implementado
    
    * Refatorando código para melhor transição entre telas
    
    * Update version
    
    * Atualizando projeto
    
    * Atualizando projeto
    
    * Update version code
    
    * Removendo arquivo da pasta kotlin
    
    * Atualizando .idea
    
    * Atualizando .idea
    
    * Atualizando dependencias e layout
    
    * Atualizando listeners

--- app/src/main/java/br/com/brunocarvalhs/paguei/features/costs/extracts/ExtractFragment.kt
+++ app/src/main/java/br/com/brunocarvalhs/paguei/features/costs/extracts/ExtractFragment.kt
@@ -3,7 +3,6 @@ package br.com.brunocarvalhs.paguei.features.costs.extracts
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.SearchView
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -42,7 +41,8 @@ class ExtractFragment : BaseFragment<FragmentExtractListBinding>(),
     override fun initView() {
         visibilityToolbar(true)
         binding.list.layoutManager = LinearLayoutManager(context)
-        binding.search.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+        binding.search.setOnQueryTextListener(object :
+            androidx.appcompat.widget.SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String?): Boolean {
                 return false
             }
