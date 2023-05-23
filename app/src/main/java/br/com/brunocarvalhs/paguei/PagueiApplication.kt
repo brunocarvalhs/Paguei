d431fbbe5a76ea3942c904cfe81878ed4853810b
Bruno Silva Carvalho <47616928+brunocarvalhs@users.noreply.github.com>
2023-05-23 19:28:58 -0300

    Update theme

--- app/src/main/java/br/com/brunocarvalhs/paguei/PagueiApplication.kt
+++ app/src/main/java/br/com/brunocarvalhs/paguei/PagueiApplication.kt
@@ -1,7 +1,13 @@
 package br.com.brunocarvalhs.paguei
 
 import androidx.multidex.MultiDexApplication
+import com.google.android.material.color.DynamicColors
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class PagueiApplication : MultiDexApplication()
+class PagueiApplication : MultiDexApplication() {
+    override fun onCreate() {
+        super.onCreate()
+//        DynamicColors.applyToActivitiesIfAvailable(this)
+    }
+}
