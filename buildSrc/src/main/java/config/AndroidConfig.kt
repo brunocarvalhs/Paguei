85135279cb26faa83676d86b516913758d213634
Bruno Silva Carvalho <47616928+brunocarvalhs@users.noreply.github.com>
2023-05-23 14:38:54 -0300

    Update version

--- buildSrc/src/main/java/config/AndroidConfig.kt
+++ buildSrc/src/main/java/config/AndroidConfig.kt
@@ -7,8 +7,8 @@ object AndroidConfig {
     const val MIN_SDK_VERSION = 24
     const val TARGET_SDK_VERSION = 33
 
-    const val VERSION_CODE = 16
-    const val VERSION_NAME = "7.1.0"
+    const val VERSION_CODE = 17
+    const val VERSION_NAME = "7.2.0"
 
     const val APP_ID = "br.com.brunocarvalhs.paguei"
     const val TEST_INSTRUMENTATION_RUNNER = "androidx.test.runner.AndroidJUnitRunner"
