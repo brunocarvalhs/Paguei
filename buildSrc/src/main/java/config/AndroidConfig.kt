b33d503c32a702ae2ff84411563c1525ae615797
Bruno Silva Carvalho <47616928+brunocarvalhs@users.noreply.github.com>
2023-06-13 17:57:13 -0300

    Update version

--- buildSrc/src/main/java/config/AndroidConfig.kt
+++ buildSrc/src/main/java/config/AndroidConfig.kt
@@ -1,14 +1,15 @@
 package config
 
 import org.gradle.api.JavaVersion
+import java.io.File
 
 object AndroidConfig {
     const val COMPILE_SDK_VERSION = 33
     const val MIN_SDK_VERSION = 24
     const val TARGET_SDK_VERSION = 33
 
-    const val VERSION_CODE = 21
-    const val VERSION_NAME = "8.1.2"
+    const val VERSION_CODE = 22
+    const val VERSION_NAME = "8.2.0"
 
     const val APP_ID = "br.com.brunocarvalhs.paguei"
     const val TEST_INSTRUMENTATION_RUNNER = "androidx.test.runner.AndroidJUnitRunner"
