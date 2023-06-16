33ff1d0f38430299343e8f300a787ec70626a219
Bruno Silva Carvalho <47616928+brunocarvalhs@users.noreply.github.com>
2023-06-16 18:40:47 -0300

    feat(performance): Implementation dependencies (#67)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,3 @@
-buildscript {
-    dependencies {
-        classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.5")
-    }
-}
-
 plugins {
     id("com.android.application") version "7.3.1" apply false
     id("com.android.library") version "7.3.1" apply false
@@ -12,6 +6,8 @@ plugins {
     id("org.jetbrains.kotlin.plugin.serialization") version "1.8.0" apply false
     id("com.google.dagger.hilt.android") version "2.46" apply false
     id("com.google.gms.google-services") version "4.3.15" apply false
+    id("com.google.firebase.crashlytics") version "2.9.5" apply false
+    id("com.google.firebase.firebase-perf") version "1.4.2" apply false
     id("androidx.navigation.safeargs") version "2.5.3" apply false
 }
 
