fc7ab44fd4eabfcef49232fd6b13ff5a4d1dafe1
Bruno Silva Carvalho <47616928+brunocarvalhs@users.noreply.github.com>
2023-07-19 11:38:04 -0300

    feat(checkList): update versions dependencies

--- buildSrc/src/main/java/dependencies/Versions.kt
+++ buildSrc/src/main/java/dependencies/Versions.kt
@@ -4,20 +4,20 @@ object Versions {
     object Core {
         const val MULTIDEX = "2.0.1"
         const val KTX = "1.9.0"
-        const val LIFECYCLE_RUNTIME = "2.5.1"
+        const val LIFECYCLE_RUNTIME = "2.6.1"
     }
 
     object UI {
-        const val MATERIAL = "1.7.0"
-        const val FRAGMENT_KTX = "1.5.5"
-        const val LIFECYCLE_VIEWMODEL_KTX = "2.5.1"
-        const val APPCOMPAT = "1.5.1"
-        const val DATABINDING_COMMON = "7.4.0"
-        const val DATABINDING_COMPILER = "7.4.0"
+        const val MATERIAL = "1.9.0"
+        const val FRAGMENT_KTX = "1.6.0"
+        const val LIFECYCLE_VIEWMODEL_KTX = "2.6.1"
+        const val APPCOMPAT = "1.6.1"
+        const val DATABINDING_COMMON = "8.0.2"
+        const val DATABINDING_COMPILER = "8.0.2"
         const val CONSTRAINT_LAYOUT = "2.1.4"
         const val COORDINATOR_LAYOUT = "1.2.0"
-        const val NAVIGATION_FRAGMENT = "2.5.3"
-        const val NAVIGATION_UI = "2.5.3"
+        const val NAVIGATION_FRAGMENT = "2.6.0"
+        const val NAVIGATION_UI = "2.6.0"
     }
 
     object Test {
@@ -28,7 +28,7 @@ object Versions {
     }
 
     object Navigation {
-        const val NAVIGATION_RUNTIME = "2.5.3"
+        const val NAVIGATION_RUNTIME = "2.6.0"
     }
 
     object Hilt {
@@ -38,18 +38,18 @@ object Versions {
     }
 
     object Firebase {
-        const val BOM = "31.1.1"
+        const val BOM = "32.2.0"
         const val CRASHLYTICS = ""
         const val ANALYTICS = ""
-        const val AUTH = "21.1.0"
-        const val FIRESTORE = "24.4.1"
-        const val PLAY_SERVICES_AUTH = "18.1.0"
-        const val COROUTINES_PLAY_SERVICES = "1.6.4"
+        const val AUTH = "22.1.0"
+        const val FIRESTORE = "24.7.0"
+        const val PLAY_SERVICES_AUTH = "20.6.0"
+        const val COROUTINES_PLAY_SERVICES = "1.7.2"
         const val UI_AUTH = "8.0.2"
     }
 
     object Network {
-        const val GSON = "2.9.0"
+        const val GSON = "2.10.1"
         const val RETROFIT = "2.9.0"
         const val RETROFIT_GSON = "2.9.0"
         const val OKHTTP = "4.9.0"
