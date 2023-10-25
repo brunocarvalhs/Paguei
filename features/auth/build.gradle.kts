13b7696f682d4e937aedd1dde549c6465646735f
Bruno Silva Carvalho <bruno.silva.carvalho@outlook.com.br>
2023-10-25 16:35:12 -0300

    feat(test_ui): dependencies include

--- features/auth/build.gradle.kts
+++ features/auth/build.gradle.kts
@@ -1,6 +1,5 @@
+
 import config.AndroidConfig
-import flavor.BuildTypeDebug
-import flavor.BuildTypeRelease
 
 plugins {
     id("com.android.library")
@@ -125,4 +124,11 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
     implementation("androidx.compose.runtime:runtime-livedata:1.4.3")
     implementation("androidx.navigation:navigation-compose:2.6.0")
+
+    // Espresso
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.fragment:fragment-testing:1.6.1")
+    testImplementation("org.mockito.kotlin:mockito-kotlin:3.2.0")
+    implementation("androidx.compose.ui:ui-test-junit4:1.5.4")
 }
