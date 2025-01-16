88a1d1db4e490937e7f580a76544f23828f2a4e1
Bruno Carvalho <bruno.silva.carvalho@outlook.com.br>
2025-01-15 22:55:29 -0300

    Update AndroidConfig.kt

--- buildSrc/src/main/java/config/AndroidConfig.kt
+++ buildSrc/src/main/java/config/AndroidConfig.kt
@@ -4,9 +4,9 @@ import org.gradle.api.JavaVersion
 import java.io.File
 
 object AndroidConfig {
-    const val COMPILE_SDK_VERSION = 35
+    const val COMPILE_SDK_VERSION = 34
     const val MIN_SDK_VERSION = 24
-    const val TARGET_SDK_VERSION = 35
+    const val TARGET_SDK_VERSION = 34
 
     const val VERSION_CODE = 29
     const val VERSION_NAME = "10.0.0"
