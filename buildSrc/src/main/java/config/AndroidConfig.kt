78038c739f45f5d528ccc8c20a25945028c1f724
Bruno Carvalho <bruno.silva.carvalho@outlook.com.br>
2025-01-22 19:50:10 -0300

    Update AndroidConfig.kt

--- buildSrc/src/main/java/config/AndroidConfig.kt
+++ buildSrc/src/main/java/config/AndroidConfig.kt
@@ -4,11 +4,11 @@ import org.gradle.api.JavaVersion
 import java.io.File
 
 object AndroidConfig {
-    const val COMPILE_SDK_VERSION = 34
+    const val COMPILE_SDK_VERSION = 35
     const val MIN_SDK_VERSION = 24
-    const val TARGET_SDK_VERSION = 34
+    const val TARGET_SDK_VERSION = 35
 
-    const val VERSION_CODE = 29
+    const val VERSION_CODE = 30
     const val VERSION_NAME = "10.0.0"
 
     const val APP_ID = "br.com.brunocarvalhs.paguei"
