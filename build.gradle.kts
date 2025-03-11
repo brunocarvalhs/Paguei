a6281102a3bbdd0b5235f70f0b804823dc1e5098
Bruno Carvalho <bruno.silva.carvalho@outlook.com.br>
2025-03-11 18:51:28 -0300

    feat: atualizando gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("com.android.application") version "7.4.2" apply false
-    id("com.android.library") version "7.4.2" apply false
+    id("com.android.application") version "8.8.1" apply false
+    id("com.android.library") version "8.8.1" apply false
 
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
     id("org.jetbrains.kotlin.jvm") version "1.8.10" apply false
